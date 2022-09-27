8abc7822cb4bdd89231049d834bcfba98bdb5dea
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-09-27 10:56:14 +0100

    Add: teacher.rb

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require 'Person'
+
+# Defines the Teacher class
+class Teacher < Person
+  def initialize(specialization)
+    super()
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
