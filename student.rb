50dda01c2e64a72ca6eee87f5c834f755e4f12ad
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-09-30 21:36:28 +0100

    Add: student.rb file

---
+++ student.rb
@@ -0,0 +1,20 @@
+require_relative 'person'
+require_relative 'classroom'
+
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hockey
+    '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
