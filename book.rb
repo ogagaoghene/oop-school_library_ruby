a4bc1bf6065360c4e23909cba08fa0d8ef4090d5
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-10-03 13:36:29 +0100

    Add: book.rb, capitalize.rb, classroom.rb, decorator.rb, person.rb, rental.rb, student.rb, teacher.rb, and trimmer.rb files

---
+++ book.rb
@@ -0,0 +1,21 @@
+require_relative 'rental'
+
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rental.push(rental)
+    rental.book = self
+  end
+
+  def save_rental(rental)
+    @rentals.push(rental)
+  end
+end
