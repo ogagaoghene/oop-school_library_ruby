7be58815ec8809542bf9a3e7aef4c1b00b75a292
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-10-05 14:55:23 +0100

    Add test for student

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,7 +1,13 @@
 require './student'
 
-RSpec.describe 'Student' do 
-  it '' do 
-    # Write test here 
-  end 
-end 
+describe Student do
+  context 'Student class' do
+    before(:each) do
+      @student = Student.new(22, true)
+    end
+    it 'should tell the person age and parent permission' do
+      expect(@student).to have_attributes(age: 22, parent_permission: true)
+    end
+  end
+end
+
