8717e70a0c4f049a07b5d0f25544daaba8361c64
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-10-05 14:53:38 +0100

    Add test for book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,18 @@
 require './book'
 
-RSpec.describe 'Book' do 
-  it '' do 
-    # Write test here 
-  end 
-end 
+describe 'Book' do
+  context 'Book Class' do
+    before :each do
+      @book = Book.new('Appreciate Always', 'David Great')
+    end
+    it 'takes two parameters and returns a Book object' do
+      expect(@book).to be_an_instance_of Book
+    end
+    it 'returns the correct title' do
+      expect(@book.title).to eql 'Appreciate Always'
+    end
+    it 'returns the correct author' do
+      expect(@book.author).to eql 'David Great'
+    end
+  end
+end
